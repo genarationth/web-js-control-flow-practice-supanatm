c2fb2d33dd49980819679e4a16b4eea182474f59
supanatm <spnstation8@gmail.com>
2023-03-13 14:46:43 +0700

    ternary

---
+++ ternary.js
@@ -0,0 +1,5 @@
+const password = "";
+
+password.length >= 12 ? console.log("password is very strong so WELCOME!!!")
+: password.length >= 8 ? console.log("WELCOME!!!")
+: console.log("SORRY!!! the password is too short");
